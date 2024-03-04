c0416398a741537e42b7f6099162795b057ff307
JulianPlatz <julian.platz@icloud.com>
2024-03-04 21:20:29 +0100

    Initial Commit

---
+++ IncreaseClicker/IncreaseClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncreaseClickerApp.swift
+//  IncreaseClicker
+//
+//  Created by Julian Platz on 04.03.24.
+//
+
+import SwiftUI
+
+@main
+struct IncreaseClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
